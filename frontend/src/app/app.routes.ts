79ffba2014c013302850790118ec12fd948c909e
PhilipFalla <philipfalla@ufm.edu>
2025-11-21 01:46:43 -0600

    Working simulated login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { AuthComponent } from './pages/auth/auth';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  {
-    path: 'productos/:category',
-    loadComponent: () =>
-      import('./pages/products-list/products-list')
-        .then(m => m.ProductsListComponent)
+  { path: 'auth', component: AuthComponent },
+  { path: 'productos/:category', loadComponent: () =>
+      import('./pages/products-list/products-list').then(m => m.ProductsListComponent)
   }
 ];
--- frontend/src/app/shared/header-nav/header-nav.ts
+++ frontend/src/app/shared/header-nav/header-nav.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header-nav',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header-nav.html',
-  styleUrl: './header-nav.css',
+  styleUrl: './header-nav.css'
 })
-export class HeaderNav {}
+export class HeaderNav {
+  isLoggedIn$: Observable<boolean>;
+
+  constructor(private authService: AuthService) {
+    this.isLoggedIn$ = this.authService.isLoggedIn$;
+  }
+}
